58f61107e7797f09f18c24d132fd157bd1bf1087
Hudson Akridge <hudson.akridge@vauto.com>
2015-02-23 20:04:49 -0600

    Initial commit. I think it might work. Going to run through and check

---
+++ XmlEquivalencyComparisonTool/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace XmlEquivalencyComparisonTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonCompare = new System.Windows.Forms.Button();
+            this.tbFirstXml = new System.Windows.Forms.TextBox();
+            this.tbSecondXml = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonCompare
+            // 
+            this.buttonCompare.Location = new System.Drawing.Point(91, 226);
+            this.buttonCompare.Name = "buttonCompare";
+            this.buttonCompare.Size = new System.Drawing.Size(75, 23);
+            this.buttonCompare.TabIndex = 0;
+            this.buttonCompare.Text = "Compare";
+            this.buttonCompare.UseVisualStyleBackColor = true;
+            this.buttonCompare.Click += new System.EventHandler(this.buttonCompare_Click);
+            // 
+            // tbFirstXml
+            // 
+            this.tbFirstXml.Location = new System.Drawing.Point(30, 31);
+            this.tbFirstXml.Multiline = true;
+            this.tbFirstXml.Name = "tbFirstXml";
+            this.tbFirstXml.Size = new System.Drawing.Size(210, 74);
+            this.tbFirstXml.TabIndex = 1;
+            // 
+            // tbSecondXml
+            // 
+            this.tbSecondXml.Location = new System.Drawing.Point(30, 134);
+            this.tbSecondXml.Multiline = true;
+            this.tbSecondXml.Name = "tbSecondXml";
+            this.tbSecondXml.Size = new System.Drawing.Size(210, 86);
+            this.tbSecondXml.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.tbSecondXml);
+            this.Controls.Add(this.tbFirstXml);
+            this.Controls.Add(this.buttonCompare);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCompare;
+        private System.Windows.Forms.TextBox tbFirstXml;
+        private System.Windows.Forms.TextBox tbSecondXml;
+    }
+}
+
---
+++ XmlEquivalencyComparisonTool/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace XmlEquivalencyComparisonTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCompare_Click(object sender, EventArgs e)
+        {
+            var docOne = XDocument.Parse(tbFirstXml.Text);
+            var docTwo = XDocument.Parse(tbSecondXml.Text);
+
+            //Build element list for each document
+            var elements = docOne.Elements();
+        }
+
+        public class ComparisonXmlElement
+        {
+            public XElement Element { get; set; }
+            public IDictionary<string, ComparisonXmlAttribute> Attributes { get; set; }
+
+            public ComparisonXmlElement(XElement element)
+            {
+                Element = element;
+                Attributes = element.Attributes().ToDictionary(x => x.Name.ToString().ToLower(), x => new ComparisonXmlAttribute(x));
+                Children = BuildChildren(element);
+            }
+
+            public List<ComparisonXmlElement> Children { get; set; }
+
+            public List<ComparisonXmlElement> BuildChildren(XElement element)
+            {
+                return !element.HasElements ?
+                    new List<ComparisonXmlElement>()
+                    : element.Elements()
+                    .Select(x => new ComparisonXmlElement(x))
+                    .ToList();
+            }
+
+            public IList<AreEquivalentResponse> IsElementEquivalent(ComparisonXmlElement toCompare)
+            {
+                var missingAttributes = toCompare.Attributes.Keys.Except(Attributes.Keys);
+                if (missingAttributes.Any())
+                {
+                    return new[] { new AreEquivalentResponse(false, string.Format("Attributes missing: {0}", missingAttributes.Aggregate((x, y) => x + ", " + y))) };
+                }
+
+                var toCompareAttributes = toCompare.Attributes;
+
+                var responses = new List<AreEquivalentResponse>();
+                foreach (var attribute in Attributes)
+                {
+                    var toCompareAttribute = toCompareAttributes[attribute.Key];
+                    var response = attribute.Value.AreAttributesEquivalent(toCompareAttribute);
+
+                    if (response.Equivalent) { continue; }
+                    responses.Add(response);
+                }
+
+                return responses;
+            }
+        }
+
+        public class ComparisonXmlAttribute
+        {
+            public XAttribute Attribute { get; set; }
+
+            public ComparisonXmlAttribute(XAttribute attribute)
+            {
+                Attribute = attribute;
+            }
+
+            public AreEquivalentResponse AreAttributesEquivalent(ComparisonXmlAttribute toCompare)
+            {
+                var toCompareAttribute = toCompare.Attribute;
+
+                return toCompareAttribute.Value != Attribute.Value
+                    ? new AreEquivalentResponse(false, string.Format("Attribute values for {0} are different. Expected: {1}, Actual: {2}", Attribute.Name, Attribute.Value, toCompareAttribute.Value))
+                    : new AreEquivalentResponse(true);
+            }
+        }
+
+        public class AreEquivalentResponse
+        {
+            public AreEquivalentResponse(bool equivalent, string reason = "", AreEquivalentResponse innerResponse = null)
+            {
+                Equivalent = equivalent;
+                Reason = reason;
+                InnerResponse = innerResponse;
+            }
+
+            public bool Equivalent { get; set; }
+            public string Reason { get; set; }
+            public AreEquivalentResponse InnerResponse { get; set; }
+        }
+    }
+}
